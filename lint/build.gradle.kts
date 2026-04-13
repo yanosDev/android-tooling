b09ba17c35c26865a51a99596eac5a8f8b41f3f0
Sonay Sengün <sonay@chrono24.com>
2026-04-13 21:42:06 +0200

    Fix libs

--- lint/build.gradle.kts
+++ lint/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    alias(libs.plugins.maven.vanniktech)
+
     id(libs.plugins.lint.get().pluginId)
     `kotlin-dsl`
+    `maven-publish`
 }
 
 group = "de.yanosdev"
@@ -32,36 +33,5 @@ gradlePlugin {
 tasks.withType<Jar>().configureEach {
     manifest {
         attributes["Lint-Registry-v2"] = "de.yanosdev.lint.YanosDevIssueRegistry"
-    }
-}
-
-mavenPublishing {
-    publishToMavenCentral()
-    signAllPublications()
-
-    coordinates("io.github.yanosdev", "lint", libs.versions.yd.lint.get())
-
-    pom {
-        name = "YanosDev Lint"
-        description = "Custom lint rules for YanosDev projects"
-        url = "https://github.com/yanosdev/android-tooling"
-
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-        developers {
-            developer {
-                id = "yanosdev"
-                name = "YanosDev"
-            }
-        }
-        scm {
-            connection = "scm:git:github.com/yanosdev/android-tooling.git"
-            developerConnection = "scm:git:ssh://github.com/yanosdev/android-tooling.git"
-            url = "https://github.com/yanosdev/android-tooling"
-        }
     }
 }
