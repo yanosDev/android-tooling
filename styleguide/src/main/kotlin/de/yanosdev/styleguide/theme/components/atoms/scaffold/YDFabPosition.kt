8159ce51bc3df21820f43058cb9b2d19812b066d
Sonay Sengün <sonay@chrono24.com>
2026-04-19 19:57:22 +0200

    Implement Scaffold

---
+++ styleguide/src/main/kotlin/de/yanosdev/styleguide/theme/components/atoms/scaffold/YDFabPosition.kt
@@ -0,0 +1,7 @@
+@file:YDRevisionIn(implementedAt = "2026-04-19", revisionAfterInDays = 365)
+
+package de.yanosdev.styleguide.theme.components.atoms.scaffold
+
+import de.yanosdev.annotation.YDRevisionIn
+
+enum class YDFabPosition { Center, End }
